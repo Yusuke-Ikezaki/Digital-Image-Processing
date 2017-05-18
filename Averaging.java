0f6ff4109883bae5a45cdde7f36498934b84880d
Yusuke Ikezaki <IappleY08@gmail.com>
2017-05-18 19:55:36 +0900

    Add files
    Prewitt, Smoother, Averaging, Gaussian

---
+++ Averaging.java
@@ -0,0 +1,18 @@
+public class Averaging extends Smoother{
+    /* parameter of filter */
+    private int weight=1;
+
+    /* constructor */
+    public Averaging(int size){
+	this.size=size;
+	sum=size*size;
+    }
+
+    /* filter */
+    public int process(int x,int y){
+	return weight;
+    }
+    public int getSum(){
+	return sum;
+    }
+}
---
+++ Gaussian.java
@@ -0,0 +1,36 @@
+public class Gaussian extends Smoother{
+    /* parameter of filter */
+    private int[][] weight;
+    private int center;
+
+    /* constructor */
+    public Gaussian(int size){
+	this.size=size;
+	center=size/2;
+	int[] lookup=make_lookup(size);
+	weight=new int[size][size];
+        for(int i=0;i<size;i++)
+	    for(int j=0;j<size;j++){
+		weight[i][j]=lookup[i]*lookup[j];
+		sum+=weight[i][j];
+	    }
+    }
+
+    /* make lookup table */
+    private int[] make_lookup(int n){
+	int[][] tpas=new int[n][n];
+	for(int i=0;i<n;i++)
+	    for(int j=0;j<=i;j++){
+		if(j==0||j==i) tpas[i][j]=1;
+		else tpas[i][j]=tpas[i-1][j-1]+tpas[i-1][j];
+	    }
+	return tpas[n-1];
+    }
+    /* filter */
+    public int process(int x,int y){
+	return weight[y+center][x+center];
+    }
+    public int getSum(){
+	return sum;
+    }
+}
