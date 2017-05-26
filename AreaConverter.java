9fb25c42c25e5b316dd713b22a2aaeec20ab73f9
Yusuke Ikezaki <IappleY08@gmail.com>
2017-05-27 00:49:52 +0900

    Update AreaConverter

--- AreaConverter.java
+++ AreaConverter.java
@@ -1,8 +1,12 @@
 public abstract class AreaConverter extends Converter{
     /* operation for spatial filtering */
-    public void operate(int x,int y){
-	int rgb=filtering(x,y);
-	setRGB(x,y,rgb);
+    public void processImage(){
+	int frame=size/2;
+	for(int y=frame;y<getHeight()-frame;y++)
+	    for(int x=frame;x<getWidth()-frame;x++){
+		int rgb=filtering(x,y);
+		setRGB(x,y,rgb);
+	    }
     }
     public abstract int filtering(int x,int y);
 }
